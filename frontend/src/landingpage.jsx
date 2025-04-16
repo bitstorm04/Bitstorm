a0d5ee62c8fe6813b51df5b0b49e7e2f44c618bd
09viv <vivekgazalwar@gmail.com>
2025-04-16 16:04:33 +0530

    footer

--- frontend/src/landingpage.jsx
+++ frontend/src/landingpage.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Header from './header'
+import Footer from '../footer'
 
 export default function landingpage(){
     return (
         <div>
             <Header/>
-            
+            <Footer/>
         </div>
     )
 }
